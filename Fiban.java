74c4f38a0eedd3b04fde4a1d72690783e98b63c3
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-08-22 23:29:22 +0530

    fibonacciSeries

---
+++ Fiban.java
@@ -0,0 +1,30 @@
+public class Fiban {
+   
+    static int fib(int n)
+    {
+       
+        if (n <= 1) {
+         // base condition
+            return n;}
+          
+
+       
+        return fib(n - 1) + fib(n - 2);
+    }
+
+   
+    public static void main(String args[])
+    {
+       
+        int N = 2;
+
+       
+        for (int i = 0; i < N; i++) {
+
+            int res=fib(i);
+            System.out.print(res);
+        }
+    }
+}
+    
+
