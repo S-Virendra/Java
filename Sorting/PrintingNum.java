72f6fc4dae60e4d4739289670924a02a7eecc5be
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-08-23 22:15:05 +0530

    mergeSort

---
+++ Sorting/PrintingNum.java
@@ -0,0 +1,17 @@
+public class PrintingNum {
+
+      public static void printNo(int n){
+        for(int i=n;i<=0;i--){
+            System.out.println(i);
+        }
+
+      }
+
+    public static void main(String[] args) {
+        int n=5;
+     printNo(5);  
+
+        
+    }
+    
+}
