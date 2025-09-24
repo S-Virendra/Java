b47073dca9c9ddf42ea75c349278e7aff41f95e6
VIRENDRA SINGH PAL <palvirendrasingh697@gmail.com>
2025-09-24 23:43:05 +0530

    TwoSum

---
+++ TwoSum.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        // Map to store number and its index
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            // Check if complement exists in map
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i}; // return indices
+            }
+
+            // Store number and its index
+            map.put(nums[i], i);
+        }
+
+        // Return empty array if no solution (edge case)
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        if (result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found!");
+        }
+    }
+}
+
